8a9b265348469acf4543d8216876c3fa30f6ff62
lykasov_am <lykasov_am@lexema.ru>
2024-07-15 18:29:38 +0500

    ADD: AUTH checker

---
+++ src/components/special/AuthCheck.tsx
@@ -0,0 +1,40 @@
+import { observer } from 'mobx-react-lite';
+import { Navigate } from 'react-router-dom';
+import { useStore } from '../../features/hooks';
+
+export function CheckingAuth() {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        inset: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h3>...Loading</h3>
+    </div>
+  );
+}
+
+export const Checker = observer(({
+  children
+}: { children: JSX.Element }) => {
+  const { auth } = useStore();
+  const { isAuth, isCheckingAuth } = auth;
+
+
+  if (isAuth) return children;
+
+  if (isCheckingAuth) return <CheckingAuth />;
+
+  return (
+    <Navigate
+      replace
+      to="/authorize" // todo вопрос создавать ли логин страницу?
+    />
+  );
+})
+
+
