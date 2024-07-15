2b0814f6ce4a6ba448d2105f7a1e491be5cafb3a
lykasov_am <lykasov_am@lexema.ru>
2024-07-15 18:30:25 +0500

    CHORE: boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
+import { locale } from 'moment';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Router } from './components';
+import { StoreProvider, ThemeProvider } from './features/providers';
+import RootStore from './stores/root.store';
+
+const Store = new RootStore()
+locale('ru');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <StoreProvider store={Store}>
+        <Router />
+      </StoreProvider>
+    </ThemeProvider>
   );
 }
 
