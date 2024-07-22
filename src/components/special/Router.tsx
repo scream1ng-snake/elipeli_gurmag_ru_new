ba2953664a366196c30fe8376eadf8a069f0b8fe
lykasov_am <lykasov_am@lexema.ru>
2024-07-23 00:53:28 +0500

    DEL unused code

--- src/components/special/Router.tsx
+++ src/components/special/Router.tsx
@@ -7,9 +7,6 @@ import { FC } from 'react';
 import { Checker } from './AuthCheck';
 import MainPage from '../pages/main/MainPage';
 import SelectReception from '../pages/reception/SelectReception';
-import { observer } from 'mobx-react-lite';
-import { useStore } from '../../features/hooks';
-import { logger } from '../../features/logger';
 
 const routes: Array<{
   path: string,
@@ -28,25 +25,18 @@ const routes: Array<{
     }
   ]
 
-export const RouterComponent: FC = observer(() => {
-  const { nav: { location }} = useStore()
-  logger.log('location changed ' + location.pathname, 'Router')
-  return (
-    <BrowserRouter>
-      <Routes location={location}>
-        {routes.map((route) =>
-          <Route
-            key={route.path}
-            path={route.path}
-            element={route.private
-              ? <Checker>{route.element}</Checker>
-              : route.element
-            }
-          />
-        )}
-      </Routes>
-    </BrowserRouter>
-  )
-})
-
+export const RouterComponent: FC = () => <BrowserRouter>
+  <Routes>
+    {routes.map((route) =>
+      <Route
+        key={route.path}
+        path={route.path}
+        element={route.private
+          ? <Checker>{route.element}</Checker>
+          : route.element
+        }
+      />
+    )}
+  </Routes>
+</BrowserRouter>
 
