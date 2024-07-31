52cacc439b8648c35239dd1fff8972cf9ead4243
lykasov_am <lykasov_am@lexema.ru>
2024-07-31 17:26:04 +0500

    FIX: поправил поведение размера окна на разных уст

--- src/components/layout/Wrapper.tsx
+++ src/components/layout/Wrapper.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactNode } from 'react'
 const Wrapper: FC<{ children: ReactNode }> = props => 
-  <div style={{ width: '100vw', height: '100vh' }}>
+  <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
     {props.children}
   </div>
 
