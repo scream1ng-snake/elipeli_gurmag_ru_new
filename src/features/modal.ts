40f70a736433e6ef9b940953f5562b4ead03ddc2
lykasov_am <lykasov_am@lexema.ru>
2024-08-06 16:16:39 +0500

    UPD popup util

--- src/features/modal.ts
+++ src/features/modal.ts
@@ -1,8 +1,17 @@
 import { makeAutoObservable } from "mobx";
+import { Optional, Undef } from "./helpers";
 
-export default class Popup {
+type PopupParams<Item, savedContentType = any> = Undef<{
+  onOpen?: (item: Item, save: (content: savedContentType ) => void) => any
+  onClose?: (item: Item) => any
+}>
+export default class Popup<I, I2 = any> {
+  content: Optional<I> | Undef<I> = null
+  private setContent = (content: Optional<I>) => {
+    this.content = content
+  }
   show = false
-  constructor() {
+  constructor(readonly params?: PopupParams<I, I2>) {
     makeAutoObservable(this, {}, { autoBind: true });
   }
   open() {
@@ -10,5 +19,20 @@ export default class Popup {
   }
   close() {
     this.show = false
+    this.setContent(null)
+    if(this.content) {
+      this.params?.onClose?.(this.content)
+      if(this.saved) this.saved = null
+    }
+  }
+  watch(content: I) {
+    this.setContent(content)
+    this.params?.onOpen?.(content, this.saver)
+    this.open()
+  }
+
+  saved: any = null
+  private saver(content: I2) {
+    this.saved = content
   }
 }
