4713ad117f820aa061c5a5cbaae3488ece1b9496
lykasov_am <lykasov_am@lexema.ru>
2024-08-20 21:40:16 +0500

    начал псиать корзину

---
+++ src/components/pages/cart/CartPage.tsx
@@ -0,0 +1,12 @@
+import { observer } from "mobx-react-lite"
+import { FC } from "react"
+
+const CartPage: FC = observer(() => {
+  return(
+    <div>
+      dsdsdsd
+    </div>
+  )
+})
+
+export default CartPage
