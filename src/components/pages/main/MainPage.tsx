9e29abe32f6011bfc4aee9fd85f0f4c7da7bff7d
lykasov_am <lykasov_am@lexema.ru>
2024-08-24 14:06:52 +0500

    CHORE: eslint wars mainpage

--- src/components/pages/main/MainPage.tsx
+++ src/components/pages/main/MainPage.tsx
@@ -3,26 +3,23 @@ import { FC } from "react"
 import Wrapper from "../../layout/Wrapper"
 import Collections from "./parts/Collections/Collections"
 import Cooks from "./parts/Cooks/Cooks"
-import Categories from "./parts/Menu/Menu"
 import ReceptionSwitcher from "./parts/ReceptionSwitcher/ReceptionSwitcher"
 import Stories from "./parts/Stories/Stories"
-import styles from './styles.module.css'
 import Menu from "./parts/Menu/Menu"
+import BottomNavigation from "../../common/BottomNav/BottomNav"
 
 const MainPage: FC = observer(() => {
   return <Wrapper>
     <ReceptionSwitcher />
-    <EmptyUnderFixed height="85px" />
     <Stories />
     <Collections />
     <Cooks />
     <Menu />
-
+    <BottomNavigation />
   </Wrapper>
 })
 
-const EmptyUnderFixed: FC<{ height: string }> = props =>
-  <div style={{ height: props.height }} />
+
 
 
 export default MainPage
