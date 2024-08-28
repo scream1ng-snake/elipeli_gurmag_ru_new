7a7f97b28c7fb2d1eee943d05e9973335935c1dd
lykasov_am <lykasov_am@lexema.ru>
2024-08-28 13:41:52 +0500

    FIX: костыльно поправил верстку

--- src/components/layout/Fixed.tsx
+++ src/components/layout/Fixed.tsx
@@ -1,13 +1,25 @@
+import { observer } from 'mobx-react-lite'
 import { FC, ReactNode, Fragment } from 'react'
-const Fixed: FC<{ children: ReactNode }> = props =>
-  <Fragment>
-    <div style={{ position: 'fixed', zIndex: 1001, background: 'var(--tg-theme-secondary-bg-color)' }}>
+import { useStore } from '../../features/hooks'
+const bannerHeight = 149
+const switcherHeight = 68
+const authBtnHeight = 68
+const Fixed: FC<{ children: ReactNode }> = observer(props => {
+  const { auth } = useStore()
+  return <Fragment>
+    <div style={{ position: 'fixed', zIndex: 1, background: 'var(--tg-theme-secondary-bg-color)' }}>
       {props.children}
     </div>
-    <div style={{ position: 'static', visibility: 'hidden' }}>
-      {props.children}
-    </div>
-
+    <div
+      style={{
+        height: auth.isFailed
+          ? auth.bannerToTg.show
+            ? bannerHeight + switcherHeight
+            : authBtnHeight + switcherHeight
+          : switcherHeight
+      }}
+    />
   </ Fragment>
 
+})
 export default Fixed;
