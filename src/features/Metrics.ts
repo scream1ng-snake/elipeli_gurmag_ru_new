5d2659a83a8628428dcc5d890a5238e5024b186f
lykasov_am <lykasov_am@lexema.ru>
2024-09-12 10:01:21 +0500

    UPD: metrics util

--- src/features/Metrics.ts
+++ src/features/Metrics.ts
@@ -1,6 +1,7 @@
 import { CourseItem } from "../stores/menu.store";
 import { logger } from "./logger";
 import { CouseInCart, historyOrderItem } from "../stores/cart.store";
+import config from "./config";
 
 declare global {
   interface Window {
@@ -15,71 +16,79 @@ abstract class Metrics {
   static pixelID = 3545385
 
   static buyYandex(order: historyOrderItem) {
-    window.dataLayer?.push?.({
-      "ecommerce": {
-          "currencyCode": "RUB",
-          "purchase" : {
-              "actionField" : {
-                "id": order.VCode
-              },
-              "products" : order.Courses.map(course => ({
-                "id": course.CourseCode,
-                "name": course.CourseName,
-                "price": course.CourseCost,
-                "quantity": course.CourseQuantity,
-              }))
-          }
-      }
-  });
-
-  window.ym?.(98171988,'reachGoal','purchase')
+    if(!config.isDev) {
+      window.dataLayer?.push?.({
+        "ecommerce": {
+            "currencyCode": "RUB",
+            "purchase" : {
+                "actionField" : {
+                  "id": order.VCode
+                },
+                "products" : order.Courses.map(course => ({
+                  "id": course.CourseCode,
+                  "name": course.CourseName,
+                  "price": course.CourseCost,
+                  "quantity": course.CourseQuantity,
+                }))
+            }
+        }
+    });
+  
+    window.ym?.(98171988,'reachGoal','purchase')
+    }
   }
   static buy(totalPrice: number, IDs: number[]) {
-    _tmr.push({ 
-      type: 'reachGoal', 
-      id: this.pixelID, 
-      value: totalPrice, 
-      goal: 'order', 
-      params: {
-        product_id: IDs
-      }
-    });
+    if(!config.isDev) {
+      _tmr.push({ 
+        type: 'reachGoal', 
+        id: this.pixelID, 
+        value: totalPrice, 
+        goal: 'order', 
+        params: {
+          product_id: IDs
+        }
+      });
+    }
   }
   static registration() {
-    _tmr.push({ 
-      type: 'reachGoal', 
-      id: this.pixelID, 
-      goal: 'registration'
-    })
-
-    window.ym?.(98171988,'reachGoal','registration')
+    if(!config.isDev) {
+      _tmr.push({ 
+        type: 'reachGoal', 
+        id: this.pixelID, 
+        goal: 'registration'
+      })
+  
+      window.ym?.(98171988,'reachGoal','registration')
+    }
   }
   static addToCart(course: CourseItem) {
-    logger.log('addtobasket event vk pixel', 'metrics')
-    console.log(_tmr)
-    _tmr.push({ 
-      type: 'reachGoal', 
-      id: this.pixelID, 
-      value: course.Price, 
-      goal: 'addbasket', 
-      params: { product_id: course.VCode }
-    });
-    window.dataLayer?.push?.({
-      "ecommerce": {
-          "currencyCode": "RUB",
-          "add" : {
-              "products" : [{
-                "id": course.VCode,
-                "name": course.Name,
-                "category": course.CatVCode,
-                "price": course.Price,
-                "quantity": 1,
-              }]
-          }
-      }
-    });
-    
-    window.ym?.(98171988,'reachGoal','add')
+    if(!config.isDev) {
+      logger.log('addtobasket event vk pixel', 'metrics')
+      console.log(_tmr)
+      _tmr.push({ 
+        type: 'reachGoal', 
+        id: this.pixelID, 
+        value: course.Price, 
+        goal: 'addbasket', 
+        params: { product_id: course.VCode }
+      });
+      window.dataLayer?.push?.({
+        "ecommerce": {
+            "currencyCode": "RUB",
+            "add" : {
+                "products" : [{
+                  "id": course.VCode,
+                  "name": course.Name,
+                  "category": course.CatVCode,
+                  "price": course.Price,
+                  "quantity": 1,
+                }]
+            }
+        }
+      });
+      
+      window.ym?.(98171988,'reachGoal','add')
+    }
   }
 }
 
