d12d6fdff65b5e106cfa0256cfb0ac77dee95bce
lykasov_am <lykasov_am@lexema.ru>
2024-11-02 14:20:43 +0500

    UPD: vk tunnel for dev

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './features/perfomance';
 
+import vkBridge from '@vkontakte/vk-bridge';
+
+vkBridge.send('VKWebAppInit');
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
