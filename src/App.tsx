4f736073426c45829c6e4df113c9cd45a4803d60
lykasov_am <lykasov_am@lexema.ru>
2024-11-25 14:38:24 +0500

    QWWQQWWQW

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,19 @@ import './reset.css';
 import { RouterComponent as Router } from './components';
 import { StoreProvider, ThemeProvider } from './features/providers';
 import RootStore from './stores/root.store';
+import bridge from '@vkontakte/vk-bridge';
 
 const Store = new RootStore()
 locale('ru');
+
+bridge.subscribe((e) => {
+  if (e.detail.type === 'VKWebAppLocationChanged') {
+    console.log(e.detail.data)
+  }
+  if (e.detail.type === 'VKWebAppChangeFragment') {
+    console.log(e.detail.data)
+  }
+});
 
 function App() {
   return (
